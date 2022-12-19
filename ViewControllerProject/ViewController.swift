2e8e8ff28d201381d18fc3e9967e63a1d9176668
Bedirhan Köse <bedirhan.kose05@gmail.com>
2022-12-19 23:13:15 +0100

    Initial Commit

---
+++ ViewControllerProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewControllerProject
+//
+//  Created by Bedirhan Köse on 19.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
