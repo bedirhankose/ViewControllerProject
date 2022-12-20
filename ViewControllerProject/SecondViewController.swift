59698d97a7e49d729549b57b42c75a5a2bc70b5c
Bedirhan Köse <bedirhan.kose05@gmail.com>
2022-12-20 15:30:58 +0100

    ViewController Project
    Basic iOS usages

---
+++ ViewControllerProject/SecondViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SecondViewController.swift
+//  ViewControllerProject
+//
+//  Created by Bedirhan Köse on 19.12.22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var SecondLabel: UILabel!
+    
+    @IBOutlet weak var FoundPasswordLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
--- ViewControllerProject/ViewController.swift
+++ ViewControllerProject/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var FirstLabel: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func CheckButtonTapped(_ sender: Any) {
+        
+        performSegue(withIdentifier: "toSecondVC", sender: nil)
+    }
+    
 }
 
